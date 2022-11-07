33d344df080e67beb43aae8ff1fbee99e1595b9b
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-11-07 12:25:48 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -27,23 +27,24 @@ def ingest_data():
         nrows = 2,    
     )
     head.values[1] = [str(line) for line in head.values[1]]
-
+    #Se ubica el nombre de las columnas en un solo renglon
     for i in range(head.columns.size):
         head.values[0][i]+=("_"+head.values[1][i])
-
+    #Se organiza como se exige y se asigna a una lista
     head.values[0] = [line.replace("_nan", "") for line in head.values[0]]
     head.values[0] = [line.replace(" ", "_") for line in head.values[0]]
     head.values[0] = [line.lower() for line in head.values[0]]
     df_columns = head.values[0]
-
+    #Se lee de nuevo el archivo
     dataset = pd.read_fwf(
         filename,
         widths=[9, 16, 16, 77],
         header = None,
         skip_blank_lines=False,
     )
+    #Omitiendo las lineas donde esta alojado el nombre de las columnas
     dataset = dataset.drop([0,1,2,3])
-
+    #Se asigna cada columna en un diccionario para hacer la respectiva organización
     cluster = [str(line) for line in dataset[0]]
     cant_claves = [str(line) for line in dataset[1]]
     porc_claves = [str(line) for line in dataset[2]]
@@ -78,12 +79,13 @@ def ingest_data():
             princ_claves_d[key] = princ_claves_d.get(key,"")+line+" "
         else:
             key+=1
-
+    #Se unen los diccionarios de las columnas existente para tener un archivo compuesto tupla de diccionarios
     df = cluster_d,cant_claves_d,porc_claves_d,princ_claves_d
-
+    #Se asigna el df a un DataFrame y se asignan los nombres de las columnas
     df = pd.DataFrame(df).transpose()
     df.columns = df_columns
-
+    #Se realiza limpieza de las claves principales como se solicita
+    df['principales_palabras_clave'] = [line.replace("    ", " ") for line in df['principales_palabras_clave']]
     df['principales_palabras_clave'] = [line.replace("   ", " ") for line in df['principales_palabras_clave']]
     df['principales_palabras_clave'] = [line.replace("  ", " ") for line in df['principales_palabras_clave']]
     df['principales_palabras_clave'] = [line.replace(".", "") for line in df['principales_palabras_clave']]

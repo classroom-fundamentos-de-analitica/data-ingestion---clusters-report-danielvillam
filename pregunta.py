5b3a4419da52b7410befd2937b3df5f5cc304730
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-11-07 11:35:17 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,5 +17,72 @@ def ingest_data():
     #
     # Inserte su código aquí
     #
+    #Ubicacion del archivo a leer
+    filename = 'clusters_report.txt'
+    #Se extrae el titulo o encabezado del archivo, teniendo en cuenta que es de dos renglones(filas)
+    head = pd.read_fwf(
+        filename,
+        widths=[9, 16, 16, 76],
+        header = None,
+        nrows = 2,    
+    )
+    head.values[1] = [str(line) for line in head.values[1]]
+
+    for i in range(head.columns.size):
+        head.values[0][i]+=("_"+head.values[1][i])
+
+    head.values[0] = [line.replace("_nan", "") for line in head.values[0]]
+    head.values[0] = [line.replace(" ", "_") for line in head.values[0]]
+    head.values[0] = [line.lower() for line in head.values[0]]
+    df_columns = head.values[0]
+
+    dataset = pd.read_fwf(
+        filename,
+        widths=[9, 16, 16, 77],
+        header = None,
+        skip_blank_lines=False,
+    )
+    dataset = dataset.drop([0,1,2,3])
+
+    cluster = [str(line) for line in dataset[0]]
+    cant_claves = [str(line) for line in dataset[1]]
+    porc_claves = [str(line) for line in dataset[2]]
+    princ_claves = [str(line) for line in dataset[3]]
+
+    cluster_d = {}
+    key = 1
+    for line in cluster:
+        if(line!="nan"):
+            cluster_d[key] = cluster_d.get(key,"")+line
+            key+=1
+
+    cant_claves_d = {}
+    key = 1
+    for line in cant_claves:
+        if(line!="nan"):
+            cant_claves_d[key] = cant_claves_d.get(key,"")+line
+            key+=1
+
+    porc_claves_d = {}
+    key = 1
+    for line in porc_claves:
+        if(line!="nan"):
+            porc_claves_d[key] = porc_claves_d.get(key,"")+line
+            key+=1
+
+    princ_claves_d = {}
+    key = 1
+    for line in princ_claves:
+        if(line!="nan"):
+            princ_claves_d[key] = princ_claves_d.get(key,"")+line+" "
+        else:
+            key+=1
+
+    df = cluster_d,cant_claves_d,porc_claves_d,princ_claves_d
+
+    df = pd.DataFrame(df).transpose()
+    df.columns = df_columns
+
+    df['principales_palabras_clave'] = [line.replace("  ", " ") for line in df['principales_palabras_clave']]
 
     return df
